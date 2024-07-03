ec24bb624f6e45278961dd4a159e9684c11e9f4a
Zentx <luciano.moliterno.97@gmail.com>
2024-07-03 19:28:28 -0300

    Agregado Login + configuracion

---
+++ Grupo-16-OO2-2024-main/StockSystem/src/main/java/com/unla/stocksystem/entity/User.java
@@ -0,0 +1,46 @@
+package com.unla.stocksystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "Users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idUser;
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Column(name = "userName")
+    private String userName;
+    @Column(name = "password", length = 60)
+    private String password;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idUserRole", nullable = false)
+    private UserRole userRole;
+
+    public User(int idUser, String userName, String password, UserRole userRole) {
+        super();
+        this.idUser = idUser;
+        this.userName = userName;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+}
---
+++ Grupo-16-OO2-2024-main/StockSystem/src/test/java/com/unla/stocksystem/StockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.stocksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
