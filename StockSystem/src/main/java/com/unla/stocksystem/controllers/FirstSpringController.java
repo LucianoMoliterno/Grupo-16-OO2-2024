9ed158fc55a423342fab109ad7126f2f601a63d0
BraianOtondo <braian_otondo99@outlook.com>
2024-07-04 11:41:32 -0300

    se creó el repositorio user para realizar el login

---
+++ StockSystem/src/main/java/com/unla/stocksystem/controllers/FirstSpringController.java
@@ -0,0 +1,17 @@
+package com.unla.stocksystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/firtsspring")
+public class FirstSpringController {
+	@GetMapping("/")
+	public String helloWorld() {
+		return "HelloWorld";
+	}
+	
+	
+}
