59194e0ed2eff83e03fdb325ecb80c6b4dfd8de7
BraianOtondo <braian_otondo99@outlook.com>
2024-07-07 09:00:46 -0300

    Se crea el home para la pagina

---
+++ StockSystem/src/main/java/com/unla/stocksystem/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.unla.stocksystem.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+	//@GetMapping("/login")
+	//public String login()
+}
---
+++ StockSystem/src/main/java/com/unla/stocksystem/helpers/ViewRouteHelper.java
@@ -0,0 +1,5 @@
+package com.unla.stocksystem.helpers;
+
+public class ViewRouteHelper {
+
+}
