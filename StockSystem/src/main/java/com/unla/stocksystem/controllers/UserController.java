40ebec650f764622f92e08bf0dd3a996421c00ed
BraianOtondo <braian_otondo99@outlook.com>
2024-07-09 11:20:16 -0300

    Se crea url loginn temporal con su respectivo codigo html

--- StockSystem/src/main/java/com/unla/stocksystem/controllers/UserController.java
+++ StockSystem/src/main/java/com/unla/stocksystem/controllers/UserController.java
@@ -1,9 +1,38 @@
 package com.unla.stocksystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.unla.stocksystem.helpers.ViewRouteHelper;
 
 @Controller
+//@RequestMapping("/loginn")
 public class UserController {
-	//@GetMapping("/login")
-	//public String login()
+	@GetMapping("/loginn")
+	public ModelAndView login(@RequestParam(name = "error", required = false) String error,
+			@RequestParam(name = "logout", required = false) String logout) {
+		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.USER_LOGIN);
+		modelAndView.addObject("error", error);
+		modelAndView.addObject("logout", logout);
+		return modelAndView;
+	}
+
+	/*@GetMapping("/loginsuccess")
+	public RedirectView loginCheck() {
+		return new RedirectView(ViewRouteHelper.INDEX);
+	}
+
+	@GetMapping("/logout")
+	public RedirectView logout() {
+		return new RedirectView(ViewRouteHelper.LOGOUT_SUCCESS);
+	}
+
+	@GetMapping("/logoutsuccess")
+	public ModelAndView logoutSuccess() {
+		return new ModelAndView(ViewRouteHelper.USER_LOGOUT);
+	}*/
 }
