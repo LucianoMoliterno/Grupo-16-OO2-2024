fd355c9b62cbde8580b1340673407ca8372e3adb
Zentx <luciano.moliterno.97@gmail.com>
2024-07-10 21:54:22 -0300

    Login Succefully, but not changes in DB

--- StockSystem/src/main/java/com/unla/stocksystem/entity/User.java
+++ StockSystem/src/main/java/com/unla/stocksystem/entity/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -18,42 +17,60 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @Table(name = "Users")
-
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idUser;
-    @NotBlank
-    @Size(min = 3, max = 50)
-    @Column(name = "userName")
-    private String userName;
-    @Column(name = "password", length = 60)
-    private String password;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idUserRole", nullable = false)
-    private UserRole userRole;
 
-    //constructor
-    public User(int idUser, String userName, String password, UserRole userRole) {
-        super();
-        this.idUser = idUser;
-        this.userName = userName;
-        this.password = password;
-        this.userRole = userRole;
-    }
-    
-    public String getUserName() {
-        return userName;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idUser;
 
-    public String getPassword() {
-        return password;
-    }
+	@NotBlank
+	@Size(min = 3, max = 50)
+	@Column(name = "userName")
+	private String userName;
 
-    public UserRole getUserRole() {
-        return userRole;
-    }
+	@Column(name = "password", length = 60)
+	private String password;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idUserRole", nullable = false)
+	private UserRole userRole;
+
+	// Constructor por defecto
+	public User() {
+	}
+
+	// Constructor con parámetros
+	public User(int idUser, String userName, String password, UserRole userRole) {
+		super();
+		this.idUser = idUser;
+		this.userName = userName;
+		this.password = password;
+		this.userRole = userRole;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public UserRole getUserRole() {
+		return userRole;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUserRole(UserRole userRole) {
+		this.userRole = userRole;
+	}
 
 }
