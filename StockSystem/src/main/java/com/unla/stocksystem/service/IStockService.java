7e11859654c12b7cb256414156886c8445d95ace
BraianOtondo <braian_otondo99@outlook.com>
2024-07-15 15:50:45 -0300

    Servicio prueba 1 creado

---
+++ StockSystem/src/main/java/com/unla/stocksystem/service/IStockService.java
@@ -0,0 +1,14 @@
+package com.unla.stocksystem.service;
+import java.util.List;
+import java.util.Optional;
+
+import com.unla.stocksystem.entity.Product;
+import com.unla.stocksystem.entity.Stock;
+
+public interface IStockService {
+	public List<Stock> getAll();
+	public Optional<Stock> findByIdStock(int idStoc) throws Exception;
+	public Stock insertOrUpdate(Stock stock);
+	public abstract Stock findByProduct(Product product);
+	public abstract List<Stock> traerConProductos();
+}
