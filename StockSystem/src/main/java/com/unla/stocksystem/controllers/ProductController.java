d0ecf2a2863c3afd1e65652dad0fbf87b475bf93
BraianOtondo <braian_otondo99@outlook.com>
2024-07-15 15:17:35 -0300

    link del controller creado en proceso y Service de Producto terminado

---
+++ StockSystem/src/main/java/com/unla/stocksystem/controllers/ProductController.java
@@ -0,0 +1,11 @@
+package com.unla.stocksystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/producto")
+public class ProductController {
+
+	
+}
---
+++ StockSystem/src/main/java/com/unla/stocksystem/service/IProductService.java
@@ -0,0 +1,13 @@
+package com.unla.stocksystem.service;
+import java.util.List;
+import java.util.Optional;
+
+import com.unla.stocksystem.entity.Product;
+
+public interface IProductService {
+	public List<Product> getAll();
+	public Optional<Product> findById(int idProduct) throws Exception;
+	public Optional<Product> findByName(String name) throws Exception;
+	public Product insertOrUpdate(Product product);
+	public boolean remove(int idProduct);
+}
